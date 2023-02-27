77b4b9b981240a3d587b8f06b5b040aa463e4900
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-27 17:27:59 +0530

    io stream processing - solution

--- 02-io-stream-processing/program.go
+++ 02-io-stream-processing/program.go
@@ -5,17 +5,95 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"sync"
 )
 
 func main() {
-	file, err := os.Open("data1.dat")
+	wg := &sync.WaitGroup{}
+	dataCh := make(chan int)
+
+	wg.Add(1)
+	go source("data1.dat", dataCh, wg)
+
+	wg.Add(1)
+	go source("data2.dat", dataCh, wg)
+
+	evenCh, oddCh := splitter(dataCh)
+	evenSumCh := sum(evenCh)
+	oddSumCh := sum(oddCh)
+
+	doneCh := merger(evenSumCh, oddSumCh, "result.dat")
+	wg.Wait()
+	close(dataCh)
+
+	<-doneCh
+	fmt.Println("Done!")
+}
+
+func source(fileName string, ch chan int, wg *sync.WaitGroup) {
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println("data :", scanner.Text())
+		txt := scanner.Text()
+		if val, err := strconv.Atoi(txt); err == nil {
+			ch <- val
+		}
 	}
-	fmt.Println("All the data is read!")
+	wg.Done()
+}
+
+func splitter(ch chan int) (<-chan int, <-chan int) {
+	evenCh := make(chan int)
+	oddCh := make(chan int)
+	go func() {
+		for val := range ch {
+			if val%2 == 0 {
+				evenCh <- val
+			} else {
+				oddCh <- val
+			}
+		}
+		close(evenCh)
+		close(oddCh)
+	}()
+	return evenCh, oddCh
+
+}
+
+func sum(valCh <-chan int) <-chan int {
+	resultCh := make(chan int)
+	go func() {
+		result := 0
+		for val := range valCh {
+			result += val
+		}
+		resultCh <- result
+	}()
+	return resultCh
+}
+
+func merger(evenSumCh, oddSumCh <-chan int, fileName string) <-chan struct{} {
+	doneCh := make(chan struct{})
+	go func() {
+		file, err := os.Create(fileName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer file.Close()
+		for i := 0; i < 2; i++ {
+			select {
+			case evenSum := <-evenSumCh:
+				fmt.Fprintf(file, "Even Total : %d\n", evenSum)
+			case oddSum := <-oddSumCh:
+				fmt.Fprintf(file, "Odd Total : %d\n", oddSum)
+			}
+		}
+		doneCh <- struct{}{}
+	}()
+	return doneCh
 }
