8a254d3e937a410414c857819ef4d1c8872ac5be
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-28 17:27:02 +0530

    grpc - request & response (with timeout context)

--- 05-grpc-app/client/client.go
+++ 05-grpc-app/client/client.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"grpc-app/proto"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
@@ -18,13 +21,18 @@ func main() {
 	}
 	client := proto.NewAppServiceClient(clientConn)
 	ctx := context.Background()
+	timeoutCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
 	addRequest := &proto.AddRequest{
 		X: 100,
 		Y: 200,
 	}
-	resp, err := client.Add(ctx, addRequest)
+	resp, err := client.Add(timeoutCtx, addRequest)
 	if err != nil {
-		log.Fatalln(err)
+		if code := status.Code(err); code == codes.DeadlineExceeded {
+			fmt.Println("Timeout occured")
+			return
+		}
 	}
 	fmt.Println("Add Response :", resp.GetResult())
 }
--- 05-grpc-app/server/server.go
+++ 05-grpc-app/server/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"grpc-app/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -15,15 +17,24 @@ type AppServiceImpl struct {
 }
 
 func (asi *AppServiceImpl) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResponse, error) {
-	x := req.GetX()
-	y := req.GetY()
-	fmt.Printf("Add request received for x = %d and y = %d\n", x, y)
-	result := x + y
-	fmt.Printf("Sending Add response with result = %d\n", result)
-	res := &proto.AddResponse{
-		Result: result,
+	fmt.Println("Wait for 8 seconds")
+	time.Sleep(8 * time.Second)
+	select {
+	case <-ctx.Done():
+		fmt.Println("timeout occured")
+		return nil, errors.New("timeout occured")
+	default:
+		x := req.GetX()
+		y := req.GetY()
+		fmt.Printf("Add request received for x = %d and y = %d\n", x, y)
+		result := x + y
+		fmt.Printf("Sending Add response with result = %d\n", result)
+		res := &proto.AddResponse{
+			Result: result,
+		}
+		return res, nil
 	}
-	return res, nil
+
 }
 
 func main() {
